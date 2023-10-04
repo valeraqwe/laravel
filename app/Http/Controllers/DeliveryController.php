b4307b006c9938ae07b614b5b79c518f1b977c48
valeraqwe <valera.komar.1999@gmail.com>
2023-10-04 16:14:46 +0300

    Implement sendParcelData method with validation

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\DeliveryRequest;
+use Illuminate\Support\Facades\Http;
 
-class DeliveryController extends Controller
-{
-    //
+class DeliveryController extends Controller {
+
+    public function sendParcelData(DeliveryRequest $request) {
+        $parcel = $request->input('parcel');
+        $recipient = $request->input('recipient');
+
+        $data = [
+            'customer_name' => $recipient['name'],
+            'phone_number' => $recipient['phone'],
+            'email' => $recipient['email'],
+            'sender_address' => config('app.sender_address'),
+            'delivery_address' => $recipient['address']
+        ];
+
+        $response = Http::post('novaposhta.test/api/delivery', $data);
+
+        return $response->json();
+    }
 }
